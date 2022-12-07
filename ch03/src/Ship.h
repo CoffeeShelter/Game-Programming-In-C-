834ef88b8cd7ffd78e2778f163b09ce3e9c0a3b0
CoffeeShelter <ij238950@gmail.com>
2022-12-07 18:58:53 +0900

    upload ch02

---
+++ ch03/src/Ship.h
@@ -0,0 +1,24 @@
+// ----------------------------------------------------------------
+// From Game Programming in C++ by Sanjay Madhav
+// Copyright (C) 2017 Sanjay Madhav. All rights reserved.
+//
+// Released under the BSD License
+// See LICENSE in root directory for full details.
+// ----------------------------------------------------------------
+
+#include "Actor.h"
+#include "SDL/SDL.h"
+
+class Ship : public Actor
+{
+public:
+	Ship(class Game *game);
+	void UpdateActor(float deltaTime) override;
+	void ProcessKeyboard(const uint8_t *state);
+	float GetRightSpeed() const { return mRightSpeed; }
+	float GetDownSpeed() const { return mDownSpeed; }
+
+private:
+	float mRightSpeed;
+	float mDownSpeed;
+};
